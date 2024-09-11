6a100c49aad74f9d68e663fb8a8a59c4f0441aac
Sajin-2004 <sajinjoshua123@gmail.com>
2024-09-11 09:59:50 +0530

    Add files via upload

---
+++ Selection.java
@@ -0,0 +1,36 @@
+public class Selection
+{
+public static void Selectionsort(int[]arr)
+{
+for(int i=0;i<arr.length-1;i++)
+{
+int index=i;
+for(int j=1;j<arr.length;j++)
+{
+if(arr[j]<arr[index])
+{
+index=j;
+}
+}
+int smallerNumber=arr[index];
+arr[index]=arr[i];
+arr[i]=smallerNumber;
+}
+}
+public static void main(String a[])
+{
+int[]arr1={9,14,3,2,43,11,58,22};
+System.out.println("before selection sort");
+for(int i:arr1)
+{
+System.out.print(i+"");
+}
+System.out.println();
+Selectionsort(arr1);
+System.out.println("after selection sort");
+for(int i:arr1)
+{
+System.out.print(i+"");
+}
+}
+}
